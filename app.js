d6ec584125703cacdaab4086fed0521f8448fdca
Mubashir ali <mubashir123mub456@gmail.com>
2024-09-08 19:31:42 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,39 @@
+var num1 = +prompt ('enter first number')
+var num2 = +prompt ('enter second number')
+
+document.write('the sum of ' +num1 +' and' +num2 +' is ' +Number (num1+num2))
+document.write('the subract of ' +num1 + ' and' +num2 +' is ' +Number(num1-num2))
+document.write('the product of ' +num1 + ' and' +num2 +' is ' +Number(num1*num2))
+document.write('the division of ' +num1 + ' and' +num2 +' is ' +Number(num1/num2))
+document.write('the modulus of ' +num1 + ' and' +num2 +' is ' +Number(num1%num2))
+
+var new_num
+document.write('value after variable declaration is ' + new_num)
+var new_num = 50
+document.write('initial value :' + new_num)
+++new_num
+document.write('value after increment is :' + new_num)
+new_num = new_num + 7
+document.write('value after addition is :' + new_num)
+--new_num
+document.write('value after decrement is :' + new_num)
+
+new_num2 = 3
+document.write('the reminder is :' + new_num % new_num2)
+
+var number1 = 3
+document.write('table of 3')
+document.write(' 3 x 1 = ' + number1)
+document.write(' 3 x 2 = ' + number1*2)
+document.write(' 3 x 3 = ' + number1*3)
+document.write(' 3 x 4 = ' + number1*4)
+document.write(' 3 x 5 = ' + number1*5)
+document.write(' 3 x 6 = ' + number1*6)
+document.write(' 3 x 7 = ' + number1*7)
+document.write(' 3 x 8 = ' + number1*8)
+document.write(' 3 x 9 = ' + number1*9)
+document.write(' 3 x 10 = ' + number1*10)
+
+var C_temp = 25
+var F_temp = (C_temp * 9/5) + 32
+var f_temp = 30
